8e2620c6ab3de9114d0e6ab637116cbcb9109dfb
ylgr <quyen.luminous@gmail.com>
2021-09-10 09:27:50 +0700

    (!) Refactor code

--- pallets/pallet-revenue/src/lib.rs
+++ pallets/pallet-revenue/src/lib.rs
@@ -17,14 +17,11 @@ pub mod pallet {
     pub type BalanceOf<T> =
     <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 
-    /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
     pub trait Config: frame_system::Config {
 
-        // Using for generate address to store balance
         type ModuleId: Get<ModuleId>;
 
-        // Balance handler
         type Currency: Currency<Self::AccountId> + ReservableCurrency<Self::AccountId>;
     }
 
@@ -45,13 +42,8 @@ pub mod pallet {
     #[pallet::getter(fn proposal_count)]
     pub(super) type ProposalCount<T: Config> = StorageValue<_, u32, ValueQuery>;
 
-    // Errors inform users that something went wrong.
     #[pallet::error]
     pub enum Error<T> {
-        /// Error names should be descriptive.
-        NoneValue,
-        /// Errors should have helpful documentation associated with them.
-        StorageOverflow,
         InsufficientFunds,
     }
 
@@ -61,22 +53,19 @@ pub mod pallet {
     #[pallet::call]
     impl<T: Config> Pallet<T> {
         #[pallet::weight(10_000)]
-        pub fn do_something(origin: OriginFor<T>, smart_contract_address: Vec<H160>, value: BalanceOf<T>) -> DispatchResultWithPostInfo {
+        pub fn propose(origin: OriginFor<T>, smart_contract_address: Vec<H160>, value: BalanceOf<T>) -> DispatchResultWithPostInfo {
             let who = ensure_signed(origin)?;
 
             ensure!(T::Currency::transfer(&who, &Self::account_id(), value, AllowDeath)? == (), <Error<T>>::InsufficientFunds);
 
             let new_proposal = Some((false, value, who, smart_contract_address.clone()));
-            // index proposal
+
             ProposalCount::<T>::put(Self::proposal_count().saturating_add(1));
 
-            // add above ID to ProposalID
             let new_id = Self::proposal_count();
 
-            // add new_proposal to Pending_storage_map
             ProposalMap::<T>::insert(new_id, new_proposal);
 
-            // Return a successful DispatchResultWithPostInfo
             Ok(().into())
         }
     }
